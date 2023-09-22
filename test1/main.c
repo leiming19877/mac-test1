096779d86000cba2614254c13db575ea76c8479c
mingmlei <mingmlei@tencent.com>
2023-09-22 19:55:58 +0800

    Initial Commit

---
+++ test1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test1
+//
+//  Created by mingming lei on 2023/9/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
